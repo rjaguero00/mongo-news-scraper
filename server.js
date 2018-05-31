0203af248b09b6dd7235c06e2425e228d707e1a4
Roberto aguero <rjaguero00@gmail.com>
2018-05-30 22:22:04 -0700

    commit

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with request
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("https://www.pcgamer.com/news/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
