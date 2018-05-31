0a8cc8e44e9267fd17529b2ab4c22d18d118c04e
Roberto aguero <rjaguero00@gmail.com>
2018-05-31 12:18:22 -0700

    comments

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+//Grab the articles as a json
+
 $.getJSON("/articles", function (data) {
     for (var i = 0; i < data.length; i++) {
         // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
